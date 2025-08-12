b7b084fa10c78ef630abd509ff3349df283c6fad
Ramarojaona Ravaka <ramarojaonaravaka@gmail.com>
2025-08-12 08:53:09 +0300

    set up the server

--- src/server.js
+++ src/server.js
@@ -2,12 +2,25 @@
 // Common js old one
 // Module version
 import express from "express"
+import path,{dirname} from "path"
+import { fileURLToPath } from "url"
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
+// get the filepath from URL of the current module
+const __filename = fileURLToPath(import.meta.url)
+
+// get directory name from filepath
+const __dirname = dirname(__filename)
+
+// Render the files in the public directory as static files
+app.use(express.static(path.join(__dirname,"../public")))
+// Serving the html file from the / path
+app.get("/",(req,res) => {
+    res.sendFile(path.join(__dirname,"../public","index.html"))
+})
+
 app.listen(PORT,()=>{
     console.log(`Server has been initiated on port ${PORT}`)
 })
-
-console.log("what's up")
